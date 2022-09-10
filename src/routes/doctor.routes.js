39e36c0f25213fe2a27b64970a9ce85b0e7cdae3
Rolando Sanchez <rolando.sanchez@unimart.com>
2022-09-10 16:33:32 -0600

    agrego ultimos cambios

---
+++ src/routes/doctor.routes.js
@@ -0,0 +1,149 @@
+import express from "express";
+const router = express.Router();
+
+export default function () {
+  router.get("/doctors", (req, res) => {
+    res.status(200).json({
+      code: "success",
+      data: [
+        {
+          id: 1,
+          name: "Fidel Guzman Sevilla",
+          specialities_id: [1, 2, 3],
+          descriptiom:
+            "Medico General & Ginecologia con 20 años de experiencia",
+          image: `${process.env.URL}/static/doctors/1.jpg`,
+        },
+        {
+          id: 2,
+          name: "Rolando Sanchez",
+          specialities_id: [4, 2, 5],
+          descriptiom:
+            "Medico General & Ginecologia con 20 años de experiencia",
+          image: `${process.env.URL}/static/doctors/2.jpg`,
+        },
+        {
+          id: 3,
+          name: "Roberto Gonzales",
+          specialities_id: [8, 7, 6],
+          descriptiom:
+            "Medico General & Ginecologia con 20 años de experiencia",
+          image: `${process.env.URL}/static/doctors/3.jpg`,
+        },
+        {
+          id: 4,
+          name: "Miguel Mezza",
+          specialities_id: [8, 9, 10],
+          descriptiom:
+            "Medico General & Ginecologia con 20 años de experiencia",
+          image: `${process.env.URL}/static/doctors/4.jpg`,
+        },
+        {
+          id: 5,
+          name: "Alejandro Sanchez",
+          specialities_id: [11, 12, 15],
+          descriptiom:
+            "Medico General & Ginecologia con 20 años de experiencia",
+          image: `${process.env.URL}/static/doctors/5.jpg`,
+        },
+        {
+          id: 6,
+          name: "Mario Mejia",
+          specialities_id: [16, 17, 18],
+          descriptiom:
+            "Medico General & Ginecologia con 20 años de experiencia",
+          image: `${process.env.URL}/static/centers/6.jpg`,
+        },
+        {
+          id: 7,
+          name: "Orlando Mezza",
+          specialities_id: [12, 13, 19],
+          descriptiom:
+            "Medico General & Ginecologia con 20 años de experiencia",
+          image: `${process.env.URL}/static/centers/7.jpg`,
+        },
+        {
+          id: 8,
+          name: "Santos Laguna",
+          specialities_id: [11, 14, 19],
+          descriptiom:
+            "Medico General & Ginecologia con 20 años de experiencia",
+          image: `${process.env.URL}/static/centers/8.jpg`,
+        },
+      ],
+    });
+  });
+
+  router.get("/search/doctors/:specialty_id", (req, res) => {
+    const data = [
+      {
+        id: 1,
+        name: "Fidel Guzman Sevilla",
+        specialities_id: [1, 2, 3],
+        descriptiom: "Medico General & Ginecologia con 20 años de experiencia",
+        image: `${process.env.URL}/static/doctors/1.jpg`,
+      },
+      {
+        id: 2,
+        name: "Rolando Sanchez",
+        specialities_id: [4, 2, 5],
+        descriptiom: "Medico General & Ginecologia con 20 años de experiencia",
+        image: `${process.env.URL}/static/doctors/2.jpg`,
+      },
+      {
+        id: 3,
+        name: "Roberto Gonzales",
+        specialities_id: [8, 7, 6],
+        descriptiom: "Medico General & Ginecologia con 20 años de experiencia",
+        image: `${process.env.URL}/static/doctors/3.jpg`,
+      },
+      {
+        id: 4,
+        name: "Miguel Mezza",
+        specialities_id: [8, 9, 10],
+        descriptiom: "Medico General & Ginecologia con 20 años de experiencia",
+        image: `${process.env.URL}/static/doctors/4.jpg`,
+      },
+      {
+        id: 5,
+        name: "Alejandro Sanchez",
+        specialities_id: [11, 12, 15],
+        descriptiom: "Medico General & Ginecologia con 20 años de experiencia",
+        image: `${process.env.URL}/static/doctors/5.jpg`,
+      },
+      {
+        id: 6,
+        name: "Mario Mejia",
+        specialities_id: [16, 17, 18],
+        descriptiom: "Medico General & Ginecologia con 20 años de experiencia",
+        image: `${process.env.URL}/static/centers/6.jpg`,
+      },
+      {
+        id: 7,
+        name: "Orlando Mezza",
+        specialities_id: [12, 13, 19],
+        descriptiom: "Medico General & Ginecologia con 20 años de experiencia",
+        image: `${process.env.URL}/static/centers/7.jpg`,
+      },
+      {
+        id: 8,
+        name: "Santos Laguna",
+        specialities_id: [11, 14, 19],
+        descriptiom: "Medico General & Ginecologia con 20 años de experiencia",
+        image: `${process.env.URL}/static/centers/8.jpg`,
+      },
+    ];
+
+    const filterData = data
+      .map((item) => item)
+      .filter((item) =>
+        item.specialities_id.includes(Number(req.params.specialty_id))
+      );
+
+    res.status(200).json({
+      code: "success",
+      data: filterData,
+    });
+  });
+  return router;
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import auth from "./auth.routes.js";
 import specialties from "./specialties.routes.js";
 import centers from "./centers.routes.js";
+import doctor from "./doctor.routes.js";
 
-export default { auth, specialties, centers };
+export default { auth, specialties, centers, doctor };
