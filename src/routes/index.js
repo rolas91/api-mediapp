8ab57c7145af866550e1dd92ce1ab0e54fc5939d
Rolando Sanchez <rolando.sanchez@unimart.com>
2022-09-10 13:48:33 -0600

    ultimos cambios

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import auth from "./auth.routes.js";
 import specialties from "./specialties.routes.js";
+import centers from "./centers.routes.js";
 
-export default { auth, specialties };
+export default { auth, specialties, centers };
