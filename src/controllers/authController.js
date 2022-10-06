1743584523911463c54a81e8e43ac4924a0b69f1
Rolando Sanchez <rolando.sanchez@unimart.com>
2022-10-06 14:11:58 -0600

    ultimos cambios

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -27,76 +27,6 @@ export const registerUser = async (req, res) => {
     health_code,
     specialty_id,
   } = req.body;
-  console.log(profilecode);
-  let errors = [];
-  if (!profilecode) {
-    errors.push({ code: "error", message: "Please selected profile" });
-  }
-  if (!names) {
-    errors.push({ code: "error", message: "Please add your names" });
-  }
-
-  if (names.split(" ", 4).length < 4) {
-    errors.push({ code: "error", message: "Please complete your names" });
-  }
-
-  if (!email) {
-    errors.push({ code: "error", message: "Please add your email" });
-  }
-
-  if (!password) {
-    errors.push({ code: "error", message: "Please add your password" });
-  }
-  if (!phone) {
-    errors.push({ code: "error", message: "Please add your phone" });
-  }
-  if (!birthday) {
-    errors.push({ code: "error", message: "Please add your birthday" });
-  }
-  if (!id_number) {
-    errors.push({ code: "error", message: "Please add your ID" });
-  }
-  if (!age) {
-    errors.push({ code: "error", message: "Please add your age" });
-  }
-  let profile;
-  try {
-    profile = await Profile.findOne({ where: { code: profilecode } });
-    if (
-      (profile != null || profile != undefined) &&
-      profile.code === PROFILE_DOCTOR
-    ) {
-      if (!health_code) {
-        errors.push({
-          code: "error",
-          message: "Please add your health code",
-        });
-      }
-      if (!specialty_id.length > 0) {
-        errors.push({
-          code: "error",
-          message: "Please add your specialities",
-        });
-      }
-    } else {
-      if (!blood_type) {
-        errors.push({ code: "error", message: "Please add your blood type" });
-      }
-      if (!weight) {
-        errors.push({ code: "error", message: "Please add your weight" });
-      }
-      if (!height) {
-        errors.push({ code: "error", message: "Please add your height" });
-      }
-    }
-
-    const user = await User.findOne({ where: { email: email } });
-    if (user) {
-      errors.push({ code: "error", message: "User allready register" });
-    }
-  } catch (error) {
-    res.status(500).json({ code: "error", message: error });
-  }
 
   const name = `${names.split(" ", 4)[0]} ${names.split(" ", 4)[1]}`;
   const lastname = `${names.split(" ", 4)[2]} ${names.split(" ", 4)[3]}`;
@@ -104,6 +34,14 @@ export const registerUser = async (req, res) => {
     res.status(500).json({ code: "error", errors: errors });
   } else {
     try {
+      const profile = await Profile.findOne({ where: { code: profilecode } });
+      const user = await User.findOne({ where: { email: email } });
+      if (user) {
+        res
+          .status(500)
+          .json({ code: "error", message: "User allready register" });
+      }
+
       const salt = await genSalt(10);
       password = await hash(password, salt);
       const userSaved = await User.create({
@@ -134,7 +72,6 @@ export const registerUser = async (req, res) => {
         (profile != null || profile != undefined) &&
         profile.code === PROFILE_DOCTOR
       ) {
-        console.log("entro a doc");
         await DoctorInfo.create({
           health_code: health_code,
           specialty_id: JSON.stringify(specialty_id),
