b8cd50ca097fc49f874dc8b5c7439e3c8aeca291
Rolando Sanchez <rolando.sanchez@unimart.com>
2022-10-06 13:20:58 -0600

    ultimos cambios

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ db.authenticate()
       addProfileCode();
     }, 10000);
   });
-
+//middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/static", express.static(path.join(__dirname, "/public")));
