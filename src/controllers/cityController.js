03fc6858df206fe6bab47ce24906e96692e0c76c
Rolando Sanchez <rolando.sanchez@unimart.com>
2022-10-10 08:33:10 -0600

    ultimos cambios

--- src/controllers/cityController.js
+++ src/controllers/cityController.js
@@ -3,7 +3,7 @@ import City from "../models/City.js";
 export const getCities = async (req, res, next) => {
   try {
     const response = await City.findAll({
-      where: { countryId: req.body.country_id },
+      where: { countryId: req.params.country_id },
     });
     res.status(200).json({ code: "success", cities: response });
   } catch (error) {
