632a16157c23d5d1b57447c04331f64425fc17ee
Rolando Sanchez <rsanchezbaltodano@gmail.com>
2022-12-04 03:01:05 -0600

    ultimos cambios

--- src/controllers/scheduleController.js
+++ src/controllers/scheduleController.js
@@ -1,11 +1,32 @@
 import Schedule from '../database/models/Schedule.js'
 import User from '../database/models/User.js'
+import DoctorInfo from "../database/models/DoctorInfo.js"
 import Days from '../database/models/Days.js'
 
 export const addUserSchedule = async(req, res, next) => {
     try {
-        const result = await User.findOne({where:{id:req.userid}})
-        console.log('resultado', result, req.userid);
+        const result = await User.findOne({
+            where:{id:req.userid},
+            include: [
+                {
+                  model: DoctorInfo,
+                  as: "isDoctor",
+                  attributes: ["health_code", "specialty_id", "bio"],
+                },
+                { model: Country, attributes: ["country", "code"] },
+                { model: City, as: "city", attributes: ["city"] },
+              ],
+              attributes: [
+                "id",
+                "names",
+                "lastnames",
+                "email",
+                "id_number",
+                "phone",
+                "picture",
+              ],
+        })
+       
         res.status(200).json({code:"success", data:result})
     } catch (error) {
         console.log(error)
