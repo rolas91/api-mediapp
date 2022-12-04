6ee4d233c197e70b0f6c21a2166156e1cc0614ac
Rolando Sanchez <rsanchezbaltodano@gmail.com>
2022-12-04 02:39:08 -0600

    ultimos cambios

--- src/routes/schedule.routes.js
+++ src/routes/schedule.routes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 const router = express.Router()
+import { validatedJWT } from "../middlewares/jwt-validator.js"
 import {addUserSchedule} from '../controllers/scheduleController.js'
 
 export default function(){
-    router.post("/schedule",addUserSchedule)
+    router.post("/schedule",validatedJWT,addUserSchedule)
 
     return router;
 }
