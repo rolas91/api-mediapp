c2381212e174692cc8442d13a102a63e9f472a55
Rolando Sanchez <rsanchezbaltodano@gmail.com>
2022-12-04 02:46:20 -0600

    ultimos cambios

--- src/controllers/scheduleController.js
+++ src/controllers/scheduleController.js
@@ -5,7 +5,7 @@ import Days from '../database/models/Days.js'
 export const addUserSchedule = async(req, res, next) => {
     try {
         const result = await User.findOne({where:{id:req.userid}})
-
+        console.log('resultado', result);
         res.status(200).json({code:"success", data:result})
     } catch (error) {
         console.log(error)
