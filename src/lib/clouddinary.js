82195e7778da213f9b5724d173351e1338af44ba
Rolando Sanchez <rsanchezbaltodano@gmail.com>
2022-12-13 16:12:53 -0600

    ultimos cambios

--- src/lib/clouddinary.js
+++ src/lib/clouddinary.js
@@ -1,4 +1,4 @@
-import { v2 as cloudinary } from 'cloudinary'
+import cloudinary from 'cloudinary'
 export const cloudDynari = cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
