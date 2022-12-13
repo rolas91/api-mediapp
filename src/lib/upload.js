8c11a5c2d9e60767b181386bb829bfbe178a299c
Rolando Sanchez <rsanchezbaltodano@gmail.com>
2022-12-13 16:40:38 -0600

    ultimos cambios

--- src/lib/upload.js
+++ src/lib/upload.js
@@ -1,6 +1,6 @@
 import multer from 'multer';
 import {v2 as cloudinary} from 'cloudinary'
-import {cloudinaryStorage} from 'multer-storage-cloudinary'
+import {CloudinaryStorage} from 'multer-storage-cloudinary'
 import uui from '../utils/uui.js';
 import { fileURLToPath } from 'url';
 import path, { dirname } from "path"
@@ -12,10 +12,13 @@ cloudinary.config({
     secure:true
 });
 
-var storage = cloudinaryStorage({
-    cloudinary: cloudinary,
-    folder: "demo",
-    allowedFormats: ['jpg', 'png'],
+var storage = new CloudinaryStorage({
+    cloudinary:cloudinary,
+    params:{
+        folder: 'some-folder-name',
+        format: async(req, file) => 'png',// supports promises as well
+        public_id: (req, file) => 'computed-filename-using-request',
+    },
 });
 
 export const upload = multer({ storage: storage })
